4effe1fb29135caea9228cf210cdb29e0ed31ab1
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-13 19:33:29 +0700

    add slug after create

--- app/graphql/mutations/create_question.rb
+++ app/graphql/mutations/create_question.rb
@@ -1,14 +1,14 @@
 module Mutations
   class CreateQuestion < BaseMutation
     field :message, String, null: false
+    field :slug, String, null: false
 
     argument :title, String, required: true
 
     def resolve(title:)
       current_user = ensure_current_user
-
-      current_user.questions.create(title: title)
-      { message: 'ok' }
+      question = current_user.questions.create(title: title)
+      { message: 'ok', slug: question.slug }
     end
   end
 end
