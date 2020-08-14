a831abed9a77f821c8bff2d244741e9c021dbca4
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-14 13:44:05 +0700

    add name field

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,6 +2,7 @@ module Types
   class UserType < Types::BaseObject
     field :id, Integer, null: false
     field :email, String, null: false
+    field :name, String, null: false
     # field :user_detail, UserDetail, null: false
   end
 end
